d4eb20a6df8850e30d4ae7916bb2e58422c9213f
rey2952 <rey2952@users.noreply.github.com>
2018-06-23 18:24:51 +0200

    Added image hosting

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const dir = process.cwd()
 
+const { URL } = require('url')
 const fs = require('fs')
 const path = require('path')
 const jsonfile = require('jsonfile')
+const http = require('http')
+
 const texturesDir = path.join(dir, 'textures')
 const configDir = path.join(dir, 'config.json')
+
+const textureHost = fs.existsSync(texturesDir)
 
 if (!fs.existsSync(configDir) || !jsonfile.readFileSync(configDir)) {
   console.log('No config file found...')
@@ -13,20 +18,50 @@ if (!fs.existsSync(configDir) || !jsonfile.readFileSync(configDir)) {
 
 const config = jsonfile.readFileSync(configDir)
 
-const server = require('http').createServer((req, res) => {
-  res.write(JSON.stringify(config))
-  res.end()
-})
-
+const server = http.createServer(requestHandler)
 const io = require('socket.io')(server)
 
-server.listen(config.port)
-
 io.on('connection', (socket) => {
-  console.log('hi')
+  console.log('Player Joined')
 
   socket.on('say', console.log)
 })
+
+function requestHandler(req, res) {
+  let url = new URL(path.join(`http://localhost:${config.port}`,req.url))
+  let reqPath = url.pathname.substr(1)
+  let reqOptions = url.searchParams
+
+  if (textureHost && (reqPath === 'texturelist' || reqPath === 'textures')) {
+    let fileList = fs.readdir(texturesDir, (err, fileList) => {
+      if (err) throw err
+      res.write(fileList.toString())
+      res.end()
+    })
+  } else if (textureHost && reqPath === 'texture' && reqOptions.get('name')) {
+    let filePath = path.join(texturesDir, reqOptions.get('name'))
+    fs.stat(filePath, (err, stat) => {
+      if (err == null) {
+        fs.readFile(filePath, (err, file) => {
+          if (err) throw err
+          if (file) {
+            res.write(file)
+            res.end()
+          }
+        })
+      } else {
+        res.write('File not found')
+        res.end()
+      }
+    })
+  } else if (reqPath === 'info') {
+    res.write(JSON.stringify(config))
+    res.end()
+  } else {
+    res.end()
+  }
+
+}
 
 function createConfigFile() {
   jsonfile.writeFileSync(configDir, {
@@ -35,3 +70,5 @@ function createConfigFile() {
 
   console.log('Config created!')
 }
+
+server.listen(config.port)
